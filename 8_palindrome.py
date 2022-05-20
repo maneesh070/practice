e630ffdabaaac907ef16e8d18d69effe955bbbaa
Maneesh Kumar <maneeshkumar070@gmail.com>
2022-05-20 11:26:05 +0530

    Palindrome modified

--- 8_palindrome.py
+++ 8_palindrome.py
@@ -1,9 +1,7 @@
 def PalinArrey(arr):
     for i in arr:
         arr2 = str(i)
-        if arr2[::] == arr2[::-1]:
-            continue
-        else:
+        if arr2[::] != arr2[::-1]:
             return 0
     return 1
 arr = [111, 222, 333, 444, 55455, 100]
