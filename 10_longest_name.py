042d43686c2ebabfd9117112a487ebfaee79c132
Maneesh Kumar <maneeshkumar070@gmail.com>
2022-05-21 06:37:08 +0530

    duplicate file created

--- 10_longest_name.py
+++
@@ -1,9 +0,0 @@
-def longest(names):
-    newNames = ''
-    for i in names:
-        if len(i) > len(newNames):
-            newNames = i
-    return newNames
-names = ['rohit', 'kamal', 'kamalkant', 'qwertyuio']
-displayName = longest(names)
-print(displayName)
